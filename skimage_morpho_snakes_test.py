66c972c61054a5d1f914e0b4624e954e076a198d
ambroise <ambroise.berthe@ikomia.com>
2022-06-23 10:18:09 +0200

    Add test file

---
+++ skimage_morpho_snakes_test.py
@@ -0,0 +1,24 @@
+from ikomia.core import task, ParamMap
+import os
+import cv2
+from ikomia.utils.tests import run_for_test
+import logging
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::infer skimage morpho snakes =====")
+    img = cv2.imread(data_dict["images"]["detection"]["coco"])[::-1]
+    bin_mask = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    input_img = t.getInput(0)
+    input_img.setImage(img)
+    input_mask = t.getInput(1)
+    input_mask.setImage(bin_mask)
+
+    for method in ["mgac", "mcv"]:
+        params = task.get_parameters(t)
+        params["method"] = method
+        # without update = 1, model is not updated between 2 test
+        task.set_parameters(t, params)
+        yield run_for_test(t)
+
